b737a4bef786438c79ccd9ebc2c3a7a6d82a0a2f
n1ru4l <laurinquast@googlemail.com>
2020-08-25 17:10:04 +0200

    🚓 add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const PROJECTS = false;
+const CI = !!process.env.CI;
+
+module.exports =
+  !PROJECTS || CI
+    ? require("./jest.project")({ dirname: __dirname, projectMode: PROJECTS })
+    : {
+        rootDir: __dirname,
+        projects: ["<rootDir>/packages/**/*/jest.config.js"],
+      };
