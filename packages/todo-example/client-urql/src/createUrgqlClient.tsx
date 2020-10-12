333afaaf7227d78913f450280439d5d4cf5e10b9
Laurin Quast <laurinquast@googlemail.com>
2020-10-12 06:43:37 +0100

    fix urql client

--- packages/todo-example/client-urql/src/createUrgqlClient.tsx
+++ packages/todo-example/client-urql/src/createUrgqlClient.tsx
@@ -1,81 +1,26 @@
 import { SocketIOGraphQLClient } from "@n1ru4l/socket-io-graphql-client";
 import {
-  createClient,
-  Exchange,
-  makeErrorResult,
-  OperationResult,
-  Operation,
-  makeResult,
+  Client,
+  dedupExchange,
+  cacheExchange,
+  subscriptionExchange,
 } from "urql";
-import { pipe, make, mergeMap, share, filter, takeUntil, Source } from "wonka";
-import { print } from "graphql";
 
 export const createUrqlClient = (networkInterface: SocketIOGraphQLClient) => {
-  const socketIOGraphQLUrglExchange: Exchange = ({ client }) => {
-    const createSubscriptionSource = (
-      operation: Operation
-    ): Source<OperationResult> => {
-      const observableish = networkInterface.execute({
-        operation: print(operation.query),
-        variables: operation.variables,
-      });
-
-      return make<OperationResult>(({ next, complete }) => {
-        let isComplete = false;
-        let sub: null | { unsubscribe: () => void } = null;
-
-        sub = observableish.subscribe({
-          next: (result) => next(makeResult(operation, result)),
-          error: (err) => next(makeErrorResult(operation, err)),
-          complete: () => {
-            if (!isComplete) {
-              isComplete = true;
-              if (operation.operationName === "subscription") {
-                client.reexecuteOperation({
-                  ...operation,
-                  operationName: "teardown",
-                });
-              }
-
-              complete();
-            }
-          },
-        });
-
-        return () => {
-          isComplete = true;
-          if (sub) sub.unsubscribe();
-        };
-      });
-    };
-
-    return (operations$) => {
-      const sharedOps$ = share(operations$);
-      return pipe(
-        sharedOps$,
-        mergeMap((operation) => {
-          console.log(
-            "Why is this called a lot, with 'teardown' and 'query'?",
-            operation
-          );
-
-          const { key } = operation;
-          const teardown$ = pipe(
-            sharedOps$,
-            filter((op) => op.operationName === "teardown" && op.key === key)
-          );
-
-          return pipe(
-            createSubscriptionSource(operation),
-            takeUntil(teardown$)
-          );
-        })
-      );
-    };
-  };
-
-  return createClient({
+  return new Client({
     url: "noop",
-    exchanges: [socketIOGraphQLUrglExchange],
+    exchanges: [
+      dedupExchange,
+      cacheExchange,
+      subscriptionExchange({
+        forwardSubscription: (operation) => {
+          return networkInterface.execute({
+            operation: operation.query,
+            variables: operation.variables,
+          });
+        },
+        enableAllOperations: true,
+      }),
+    ],
   });
 };
