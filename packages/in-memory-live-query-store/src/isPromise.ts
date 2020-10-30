260e4e50ef415c9d3982607bf36121bf9fb2dd11
n1ru4l <laurinquast@googlemail.com>
2020-10-31 00:33:56 +0100

    refactor: Do instanceof check for Promise.

--- packages/in-memory-live-query-store/src/isPromise.ts
+++
@@ -1,7 +0,0 @@
-import { isSome } from "./Maybe";
-
-export const isPromise = (input: unknown): input is Promise<unknown> => {
-  return (
-    isSome(input) && "then" in input && typeof input["then"] === "function"
-  );
-};
