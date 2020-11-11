d12e85b3e672b2188e9f55d17d1c0fb6e73d82a3
n1ru4l <laurinquast@googlemail.com>
2020-11-11 23:58:26 +0100

    refactor: simplify logic

--- packages/graphql-live-query-patch/src/createLiveQueryPatchDeflator.ts
+++ packages/graphql-live-query-patch/src/createLiveQueryPatchDeflator.ts
@@ -1,61 +1,46 @@
 import type { LiveExecutionResult } from "@n1ru4l/graphql-live-query";
 import { compare } from "fast-json-patch";
-import type { ExecutionResult } from "graphql";
+import type { AsyncExecutionResult, ExecutionResult } from "graphql";
 import type { ExecutionLivePatchResult } from "./ExecutionLivePatchResult";
 
 export async function* createLiveQueryPatchDeflator(
   asyncIterator: AsyncIterableIterator<LiveExecutionResult>
-): AsyncIterableIterator<ExecutionLivePatchResult | ExecutionResult> {
+): AsyncIterableIterator<
+  ExecutionLivePatchResult | ExecutionResult | AsyncExecutionResult
+> {
   let previousValue: LiveExecutionResult["data"] | null = null;
   let revision = 0;
+
   for await (const value of asyncIterator) {
-    // no live query? reset state.
+    // if it is not live we simply forward everything :)
     if (!value.isLive) {
-      previousValue = null;
-      revision = 0;
       yield value;
+      yield* asyncIterator;
+      continue;
+    }
+
+    revision++;
+
+    const valueToPublish: ExecutionLivePatchResult = { revision };
+
+    if (previousValue) {
+      const currentValue = value.data ?? {};
+      valueToPublish.patch = compare(previousValue, currentValue);
+      previousValue = currentValue;
     } else {
-      revision = revision + 1;
-
-      if (previousValue) {
-        const currentValue = value.data ?? {};
-
-        const patch = compare(previousValue, currentValue);
-        previousValue = currentValue;
-
-        const valueToPublish = {
-          revision,
-          patch,
-        } as ExecutionLivePatchResult;
-
-        if ("errors" in value) {
-          valueToPublish.errors = value.errors;
-        }
-
-        if ("extensions" in value) {
-          valueToPublish.extensions = value.extensions;
-        }
-
-        yield valueToPublish;
-      } else {
-        previousValue = value.data ?? {};
-
-        const valueToPublish = {
-          revision,
-        } as ExecutionLivePatchResult;
-
-        if ("data" in value) {
-          valueToPublish.data = previousValue;
-        }
-        if ("errors" in value) {
-          valueToPublish.errors = value.errors;
-        }
-
-        if ("extensions" in value) {
-          valueToPublish.extensions = value.extensions;
-        }
-        yield valueToPublish;
+      previousValue = value.data ?? {};
+      if ("data" in value) {
+        valueToPublish.data = previousValue;
       }
     }
+
+    if ("errors" in value) {
+      valueToPublish.errors = value.errors;
+    }
+    if ("extensions" in value) {
+      valueToPublish.extensions = value.extensions;
+    }
+
+    yield valueToPublish;
   }
 }
