6436dd7727c067f8d140b48849b331b5881e5d94
Laurin Quast <laurinquast@googlemail.com>
2020-12-18 21:30:43 +0100

    chore: fix bob config

--- bob.config.js
+++ bob.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   scope: "@n1ru4l", // Scope of organization
   ignore: [
-    "@n1ru4l/example-client",
-    "@n1ru4l/example-server",
     "@n1ru4l/todo-example-client-relay",
     "@n1ru4l/todo-example-client-apollo",
     "@n1ru4l/todo-example-client-urql",
