f7d64ccee830c28fa5a7fc9858fa2b8fcebbe8e3
Laurin Quast <laurinquast@googlemail.com>
2021-09-09 10:50:49 +0200

    chore: upgrade jest and bob

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,20 @@
-const PROJECTS = false;
+const { resolve } = require("path");
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
 const CI = !!process.env.CI;
 
-module.exports =
-  !PROJECTS || CI
-    ? require("./jest.project")({ dirname: __dirname, projectMode: PROJECTS })
-    : {
-        rootDir: __dirname,
-        projects: ["<rootDir>/packages/**/*/jest.config.js"],
-      };
+const ROOT_DIR = __dirname;
+const TSCONFIG = resolve(ROOT_DIR, "tsconfig.json");
+const tsconfig = require(TSCONFIG);
+
+module.exports = {
+  testEnvironment: "node",
+  rootDir: ROOT_DIR,
+  restoreMocks: true,
+  reporters: ["default"],
+  modulePathIgnorePatterns: ["dist", "test-assets", "test-files", "fixtures"],
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+    prefix: `${ROOT_DIR}/`,
+  }),
+  collectCoverage: false,
+  cacheDirectory: resolve(ROOT_DIR, `${CI ? "" : "node_modules/"}.cache/jest`),
+};
