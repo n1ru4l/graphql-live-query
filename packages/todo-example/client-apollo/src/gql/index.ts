345ef441b1609e674c6872d9e847c16bbb42f6fe
Laurin Quast <laurinquast@googlemail.com>
2021-09-09 12:07:52 +0200

    chore: use gql-tag-preset for apollo example

---
+++ packages/todo-example/client-apollo/src/gql/index.ts
@@ -0,0 +1,50 @@
+/* eslint-disable */
+import * as graphql from "./graphql";
+import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
+
+const documents = {
+  "\n  fragment TodoApplication_todo on Todo {\n    id\n    content\n    isCompleted\n  }\n":
+    graphql.TodoApplication_TodoFragmentDoc,
+  "\n  fragment TodoApplication_data on Query {\n    todos {\n      id\n      ...TodoApplication_todo\n    }\n  }\n":
+    graphql.TodoApplication_DataFragmentDoc,
+  "\n  mutation TodoApplication_TodoAddMutation($id: ID!, $content: String!) {\n    todoAdd(id: $id, content: $content) {\n      __typename\n    }\n  }\n":
+    graphql.TodoApplication_TodoAddMutationDocument,
+  "\n  mutation TodoApplication_TodoChangeContentMutation(\n    $id: ID!\n    $content: String!\n  ) {\n    todoChangeContent(id: $id, content: $content) {\n      __typename\n    }\n  }\n":
+    graphql.TodoApplication_TodoChangeContentMutationDocument,
+  "\n  mutation TodoApplication_TodoDeleteMutation($id: ID!) {\n    todoDelete(id: $id) {\n      __typename\n    }\n  }\n":
+    graphql.TodoApplication_TodoDeleteMutationDocument,
+  "\n  query TodoApplication_TodosQuery @live {\n    ...TodoApplication_data\n  }\n":
+    graphql.TodoApplication_TodosQueryDocument,
+  "\n  mutation TodoApplication_TodoToggleIsCompletedMutation($id: ID!) {\n    todoToggleIsCompleted(id: $id) {\n      __typename\n    }\n  }\n":
+    graphql.TodoApplication_TodoToggleIsCompletedMutationDocument,
+};
+
+export function gql(
+  source: "\n  fragment TodoApplication_todo on Todo {\n    id\n    content\n    isCompleted\n  }\n"
+): typeof documents["\n  fragment TodoApplication_todo on Todo {\n    id\n    content\n    isCompleted\n  }\n"];
+export function gql(
+  source: "\n  fragment TodoApplication_data on Query {\n    todos {\n      id\n      ...TodoApplication_todo\n    }\n  }\n"
+): typeof documents["\n  fragment TodoApplication_data on Query {\n    todos {\n      id\n      ...TodoApplication_todo\n    }\n  }\n"];
+export function gql(
+  source: "\n  mutation TodoApplication_TodoAddMutation($id: ID!, $content: String!) {\n    todoAdd(id: $id, content: $content) {\n      __typename\n    }\n  }\n"
+): typeof documents["\n  mutation TodoApplication_TodoAddMutation($id: ID!, $content: String!) {\n    todoAdd(id: $id, content: $content) {\n      __typename\n    }\n  }\n"];
+export function gql(
+  source: "\n  mutation TodoApplication_TodoChangeContentMutation(\n    $id: ID!\n    $content: String!\n  ) {\n    todoChangeContent(id: $id, content: $content) {\n      __typename\n    }\n  }\n"
+): typeof documents["\n  mutation TodoApplication_TodoChangeContentMutation(\n    $id: ID!\n    $content: String!\n  ) {\n    todoChangeContent(id: $id, content: $content) {\n      __typename\n    }\n  }\n"];
+export function gql(
+  source: "\n  mutation TodoApplication_TodoDeleteMutation($id: ID!) {\n    todoDelete(id: $id) {\n      __typename\n    }\n  }\n"
+): typeof documents["\n  mutation TodoApplication_TodoDeleteMutation($id: ID!) {\n    todoDelete(id: $id) {\n      __typename\n    }\n  }\n"];
+export function gql(
+  source: "\n  query TodoApplication_TodosQuery @live {\n    ...TodoApplication_data\n  }\n"
+): typeof documents["\n  query TodoApplication_TodosQuery @live {\n    ...TodoApplication_data\n  }\n"];
+export function gql(
+  source: "\n  mutation TodoApplication_TodoToggleIsCompletedMutation($id: ID!) {\n    todoToggleIsCompleted(id: $id) {\n      __typename\n    }\n  }\n"
+): typeof documents["\n  mutation TodoApplication_TodoToggleIsCompletedMutation($id: ID!) {\n    todoToggleIsCompleted(id: $id) {\n      __typename\n    }\n  }\n"];
+
+export function gql(source: string): unknown;
+export function gql(source: string) {
+  return (documents as any)[source] ?? {};
+}
+
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
+  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
