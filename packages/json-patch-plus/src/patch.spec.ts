9f092d0241039e958debc509d289c553abdbb13e
Laurin Quast <laurinquast@googlemail.com>
2021-09-24 21:02:05 +0200

    feat: add patch function

---
+++ packages/json-patch-plus/src/patch.spec.ts
@@ -0,0 +1,171 @@
+import { patch } from "./patch";
+
+it("applies a primitive delta (replace)", () => {
+  expect(
+    patch({
+      left: 1,
+      delta: [1, 2],
+    })
+  ).toEqual(2);
+});
+
+it("applies a primitive delta (remove)", () => {
+  expect(
+    patch({
+      left: 1,
+      delta: [1, 0, 0],
+    })
+    // the third array member 0 indicates that the value got removed instead of replaced
+  ).toEqual(undefined);
+});
+
+it("applies a primitive delta (add)", () => {
+  expect(
+    patch({
+      left: undefined,
+      delta: [1],
+    })
+  ).toEqual(1);
+});
+
+it("applies a nested delta (remove)", () => {
+  expect(
+    patch({
+      left: { a: 2 },
+      delta: { a: [2, 0, 0] },
+    })
+  ).toEqual({});
+});
+
+it("applies a nested delta (add)", () => {
+  expect(
+    patch({
+      left: {},
+      delta: { a: [2] },
+    })
+  ).toEqual({ a: 2 });
+});
+
+it("applies a deeply nested delta (replace)", () => {
+  expect(
+    patch({
+      left: { a: { a: 1 } },
+      delta: { a: { a: [1, 2] } },
+    })
+  ).toEqual({ a: { a: 2 } });
+});
+
+it("applies a deeply nested delta (remove)", () => {
+  expect(
+    patch({
+      left: { a: { a: 2 } },
+      delta: { a: [{ a: 2 }, 0, 0] },
+    })
+  ).toEqual({});
+});
+
+it("applies a deeply nested delta (add)", () => {
+  expect(
+    patch({
+      left: {},
+      delta: { a: [{ a: 2 }] },
+    })
+  ).toEqual({ a: { a: 2 } });
+});
+
+it("applies array delta (remove first member)", () => {
+  expect(
+    patch({
+      left: [1, 2],
+      delta: {
+        // indicates that this is an array operation
+        _t: "a",
+        // _ + index of the item
+        _0: [
+          // the item that is modified
+          1,
+          // 0, 0 indicates that the item got deleted
+          0, 0,
+        ],
+      },
+    })
+  ).toEqual([2]);
+});
+
+it("applies array delta (remove last member)", () => {
+  expect(
+    patch({
+      left: [1, 2],
+      delta: {
+        // indicates that this is an array operation
+        _t: "a",
+        // _ + index of the item
+        _1: [
+          // the item that is modified
+          2,
+          // 0, 0 indicates that the item got deleted
+          0, 0,
+        ],
+      },
+    })
+  ).toEqual([1]);
+});
+
+it("applies array delta (replace array member)", () => {
+  expect(
+    patch({
+      left: [1, 2],
+      delta: {
+        // indicates that this is an array operation
+        _t: "a",
+        // _ + index of the item
+        "0": [2],
+        _0: [
+          // the item that is modified
+          1,
+          // 0, 0 indicates that the item got deleted
+          0, 0,
+        ],
+      },
+    })
+  ).toEqual([2, 2]);
+});
+
+it("applies inefficient array delta", () => {
+  expect(
+    patch({
+      left: [{ a: 1 }, { a: 2 }],
+      delta: {
+        "0": {
+          a: [1, 2],
+        },
+        "1": {
+          a: [2, 1],
+        },
+        _t: "a",
+      },
+    })
+  ).toEqual([{ a: 2 }, { a: 1 }]);
+});
+
+it("applies efficient array delta (produced with objectHash function)", () => {
+  expect(
+    patch({
+      left: [{ id: 1 }, { id: 2 }],
+      delta: {
+        // item index that is moved
+        _1: [
+          // item that is moved
+          {
+            id: 2,
+          },
+          // the new index where the item is moved
+          0,
+          // indicates that this is an array move operation
+          3,
+        ],
+        _t: "a",
+      },
+    })
+  ).toEqual([{ id: 2 }, { id: 1 }]);
+});
---
+++ packages/json-patch-plus/src/patch.ts
@@ -0,0 +1,245 @@
+type Context = {
+  left: any;
+  delta: any;
+  children?: Array<Context>;
+  result: unknown;
+  name?: string | number;
+  stopped: boolean;
+  nested?: boolean;
+};
+
+export function patch(params: { left: unknown; delta: unknown }): unknown {
+  const context: Context = {
+    left: params.left,
+    delta: params.delta,
+    children: undefined,
+    result: undefined,
+    name: undefined,
+    stopped: false,
+    nested: false,
+  };
+
+  function process(context: Context) {
+    nested_collectChildrenPatchFilter(context);
+    array_collectChildrenPatchFilter(context);
+    trivial_patchFilter(context);
+    nested_patchFilter(context);
+    array_patchFilter(context);
+
+    if (context.children) {
+      for (const childrenContext of context.children) {
+        process(childrenContext);
+
+        context.result =
+          context.result ??
+          (typeof childrenContext.name === "number" ? [] : {});
+        (context.result as object)[childrenContext.name!] =
+          childrenContext.result;
+      }
+    }
+  }
+
+  process(context);
+
+  return context.result;
+}
+
+function nested_collectChildrenPatchFilter(context: Context) {
+  if (!context || !context.children) {
+    return;
+  }
+  if (context.delta._t) {
+    return;
+  }
+
+  let length = context.children.length;
+  let child;
+
+  for (let index = 0; index < length; index++) {
+    child = context.children[index];
+    if (
+      Object.prototype.hasOwnProperty.call(context.left, child.name!) &&
+      child.result === undefined
+    ) {
+      delete context.left[child.name!];
+    } else if (context.left[child.name!] !== child.result) {
+      context.left[child.name!] = child.result;
+    }
+  }
+  context.result = context.left;
+  context.stopped = true;
+}
+
+function array_collectChildrenPatchFilter(context: Context) {
+  if (!context || !context.children) {
+    return;
+  }
+  if (context.delta._t !== "a") {
+    return;
+  }
+  let length = context.children.length;
+  let child;
+  for (let index = 0; index < length; index++) {
+    child = context.children[index];
+    context.left[child.name!] = child.result;
+  }
+  context.result = context.left;
+  context.stopped = true;
+}
+
+function trivial_patchFilter(context: Context) {
+  if (typeof context.delta === "undefined") {
+    context.result = context.left;
+    context.stopped = true;
+    return;
+  }
+  context.nested = !Array.isArray(context.delta);
+  if (context.nested) {
+    return;
+  }
+  if (context.delta.length === 1) {
+    context.result = context.delta[0];
+    context.stopped = true;
+    return;
+  }
+  if (context.delta.length === 2) {
+    context.result = context.delta[1];
+    context.stopped = true;
+    return;
+  }
+  if (context.delta.length === 3 && context.delta[2] === 0) {
+    context.result = undefined;
+    context.stopped = true;
+  }
+}
+
+function nested_patchFilter(context: Context) {
+  if (!context.nested) {
+    return;
+  }
+  if (context.delta._t) {
+    return;
+  }
+  let name;
+  let child;
+  for (name in context.delta) {
+    if (context.children === undefined) {
+      context.children = [];
+    }
+    context.children.push({
+      left: context.left[name],
+      delta: context.delta[name],
+      result: undefined,
+      name,
+      stopped: false,
+    });
+  }
+  context.stopped = false;
+}
+
+const ARRAY_MOVE = 3;
+
+let compare = {
+  numerically(a: number, b: number) {
+    return a - b;
+  },
+  numericallyBy(name: string) {
+    return (a: any, b: any) => a[name] - b[name];
+  },
+};
+
+function array_patchFilter(context: Context) {
+  if (!context.nested) {
+    return;
+  }
+  if (context.delta._t !== "a") {
+    return;
+  }
+  let index;
+  let index1;
+
+  let delta = context.delta;
+  let array = context.left;
+
+  // first, separate removals, insertions and modifications
+  let toRemove = [];
+  let toInsert = [];
+  let toModify = [];
+  for (index in delta) {
+    if (index !== "_t") {
+      if (index[0] === "_") {
+        // removed item from original array
+        if (delta[index][2] === 0 || delta[index][2] === ARRAY_MOVE) {
+          toRemove.push(parseInt(index.slice(1), 10));
+        } else {
+          throw new Error(
+            `only removal or move can be applied at original array indices,` +
+              ` invalid diff type: ${delta[index][2]}`
+          );
+        }
+      } else {
+        if (delta[index].length === 1) {
+          // added item at new array
+          toInsert.push({
+            index: parseInt(index, 10),
+            value: delta[index][0],
+          });
+        } else {
+          // modified item at new array
+          toModify.push({
+            index: parseInt(index, 10),
+            delta: delta[index],
+          });
+        }
+      }
+    }
+  }
+
+  // remove items, in reverse order to avoid sawing our own floor
+  toRemove = toRemove.sort(compare.numerically);
+  for (index = toRemove.length - 1; index >= 0; index--) {
+    index1 = toRemove[index];
+    let indexDiff = delta[`_${index1}`];
+    let removedValue = array.splice(index1, 1)[0];
+    if (indexDiff[2] === ARRAY_MOVE) {
+      // reinsert later
+      toInsert.push({
+        index: indexDiff[1],
+        value: removedValue,
+      });
+    }
+  }
+
+  // insert items, in reverse order to avoid moving our own floor
+  toInsert = toInsert.sort(compare.numericallyBy("index"));
+  let toInsertLength = toInsert.length;
+  for (index = 0; index < toInsertLength; index++) {
+    let insertion = toInsert[index];
+    array.splice(insertion.index, 0, insertion.value);
+  }
+
+  // apply modifications
+  let toModifyLength = toModify.length;
+  if (toModifyLength > 0) {
+    for (index = 0; index < toModifyLength; index++) {
+      let modification = toModify[index];
+      if (context.children === undefined) {
+        context.children = [];
+      }
+      context.children.push({
+        left: context.left[modification.index],
+        delta: modification.delta,
+        name: modification.index,
+        result: undefined,
+        stopped: false,
+      });
+    }
+  }
+
+  if (!context.children) {
+    context.result = context.left;
+    context.stopped = true;
+    return;
+  }
+  context.stopped = true;
+}
